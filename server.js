a85151123542fce23171afcadf860f7e3ba4ae44
kinoflix <aligoshgar@live.com>
2026-08-07 15:29:55 +0400

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/api/chat', async (req, res) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "llama-3.3-70b-versatile", // Mükəmməl Azərbaycan dili dəstəyi olan 70B modeli
+                model: "qwen-2.5-72b-instruct", // Mükəmməl Azərbaycan dili dəstəyi olan 70B modeli
                 messages: [
                     {
                         role: "system",

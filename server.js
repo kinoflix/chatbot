73b8078b490b338b11e7f3c88d51caf5eaed31e2
kinoflix <aligoshgar@live.com>
2026-08-07 14:37:13 +0400

    Update server.js

--- server.js
+++ server.js
@@ -38,7 +38,7 @@ app.post('/api/chat', async (req, res) => {
                 "Content-Type": "application/json"
             },
             body: JSON.stringify({
-                model: "llama-3.1-8b-instant", // Ən sürətli pulsuz model
+                model: "llama-3.3-70b-versatile", // Ən sürətli pulsuz model
                 messages: [
     {
         role: "system",
